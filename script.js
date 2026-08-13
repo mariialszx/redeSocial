bfac4c0acf35106727ce963a5018ac8c91c27a4e
mariialszx <maria.luiza.bittencourt@escola.pr.gov.br>
2026-08-13 08:47:43 -0300

    ok

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-//Botão curtidas
-document.addEventListener
+//Botao curtidas
+document.addEventListener("DOMContentLoaded", () =>{
+    const likeBtn = document.querySelector(".left-actions .action-btn:first-child");
+    if (!likeBtn) return;
+    const likeSvg = likeBtn.querySelector("svg");
+
+//localiza o contador
+
+let textNode = Array
+})
+
