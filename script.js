321d5e5da5dec1ec682cc01e1812756a408dfe6e
mariialszx <maria.luiza.bittencourt@escola.pr.gov.br>
2026-08-20 08:49:21 -0300

    ok

--- script.js
+++ script.js
@@ -4,8 +4,37 @@ document.addEventListener("DOMContentLoaded", () =>{
     if (!likeBtn) return;
     const likeSvg = likeBtn.querySelector("svg");
 
-//localiza o contador
 
-let textNode = Array
+    //localiza o contador
+  
+    let textNode = Array.from(likeBtn.childNodes).find(node)=> node.nodeType
+    === Node.TEXT_NODE && node.textContent.trim()!==""
+);
+
+//zera o contador
+let cont=0;
+
+//atualiza
+if(textNode){
+    textNode.textContent = `0`;
+}
+
+//coração
+function applyLikedStyle(){
+likeSvg.style.fill = "#ef4444";
+likeSvg.style.stroke = "#ef4444";
+likeSvg.style.color = "#ef4444";
+}
+    
+//efeito curtida
+likeSvg.style.transform = "scale(1.3)";
+setTimeout(())
+
+
+
+
+
+
+
 })
 
